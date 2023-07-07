6ca4bbafa4d7825552211be78f932634a045d156
dikim <diki.liki@mologiz.com>
2023-07-07 15:45:30 +0700

    first commit 2

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'TriStateCheckbox',
-      fileName: 'vue-tri-state-checkbox'
+      name: 'ModeUi',
+      fileName: 'mode-ui'
     },
     rollupOptions: {
       external: ['vue'],
